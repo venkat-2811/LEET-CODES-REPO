08ca39bcb6ba1f3c7b62674f41684009d2b0de43
Sunkara Venkata Karthik Sai <147246984+venkat-2811@users.noreply.github.com>
2024-09-15 13:03:10 +0530

    Time: 69 ms (58.6%), Space: 18.9 MB (32.79%) - LeetHub

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        int n = s.length();
+        int mask = 0,maxlength = 0;
+        unordered_map<int,int> m;
+        m[0] = -1;
+
+        for(int i =0;i<n;i++)
+        {
+            if(s[i] == 'a')
+            {
+                mask^=(1<<0);
+            }
+            else if(s[i] == 'e')
+            {
+                mask^=(1<<1);
+            }
+            else if(s[i] == 'i')
+            {
+                mask^=(1<<2);
+            }
+            else if(s[i] == 'o')
+            {
+                mask^=(1<<3);
+            }
+            else if(s[i] == 'u')
+            {
+                mask^=(1<<4);
+            }
+
+            if(m.find(mask) !=m.end())
+            {
+                maxlength = max(maxlength,i-m[mask]);
+            }
+            else
+            {
+                m[mask] = i;
+            }
+        }
+        return maxlength;
+    }
+};
