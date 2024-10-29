1de13b0aa2aba7fd5b2865fc8d214241fd199844
Sunkara Venkata Karthik Sai <147246984+venkat-2811@users.noreply.github.com>
2024-10-29 20:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (48.42%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& A) {
+        int n = A.size();                                
+        int ind = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (A[i] < A[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+
+        if (ind == -1) {
+            reverse(A.begin(), A.end());
+            return;
+        }
+
+        for (int i = n - 1; i > ind; i--) {
+            if (A[i] > A[ind]) {
+                swap(A[i], A[ind]);
+                break;
+            }
+        }
+        reverse(A.begin() + ind + 1, A.end());
+    }
+};
